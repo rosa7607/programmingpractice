0ea46388dd5051d9ea7303accaab32a2ef3eab8b
rosa7607 <154528853+rosa7607@users.noreply.github.com>
2023-12-24 01:05:51 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.c
@@ -0,0 +1,19 @@
+int main(void){
+
+  int A, B;
+  scanf("%d %d", &A, &B);
+
+  if(A > B ){
+    printf(">");
+  }
+
+  else if(A < B){
+    printf("<");
+  }
+
+  else if (A == B){
+    printf("==");
+  }
+  
+  return 0;
+}
